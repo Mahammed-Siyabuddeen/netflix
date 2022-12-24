20a50e1b98371cd0343ad6c82fc2d4c235bfd110
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2022-12-24 12:32:03 +0530

    handlesubmit function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,25 @@
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import React, { useState } from 'react'
 import {  useNavigate } from 'react-router'
+import { auth } from '../../Firebase/Config'
 import Background from '../../images/Background.jpg'
 import NavBar from '../NavBar/NavBar'
 function Login() {
     const [email, setemail] = useState('')
     const[password,setpassword]=useState('')
+    const[processing,setProcessing]=useState(false)
    const Navigate=useNavigate()
    var handlesubmit=async(e)=>{
+       setProcessing(true)
        e.preventDefault()
-    //   const user=await signInWithEmailAndPassword(auth,email,password).then(()=>{
-    //       alert('successfully login')
-    //       Navigate('/')
-    //   })
+       signInWithEmailAndPassword(auth,email,password).then(()=>{
+          alert('successfully login')
+          Navigate('/')
+      }).catch((err)=>{
+        alert("someting went wrong please try again ")
+        window.location.reload()
+      })
+     
    }
     return (
         <div>
@@ -38,7 +46,7 @@ function Login() {
                             required
 
                         />
-                        <input style={{marginLeft:'15%'}} className='signup-btn' type="submit" value='SignIn ' />
+                        <input disabled={processing} style={{marginLeft:'15%', backgroundColor:processing?"#c79396":"#e50914"}} className='signup-btn' type="submit" value='SignIn ' />
                         <p style={{marginLeft:'11%'}}>New to Netflix? <span 
                         onClick={()=>Navigate('/signup')} className='Login-now'>Signup Now</span></p>
                     </form>
